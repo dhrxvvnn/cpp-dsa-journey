ba434e2f0cf9d1cc5ba71bfc87734c2d54964bdc
dhrxvvnn <dhruvin126gm@gmail.com>
2026-08-31 10:24:29 +0530

    q2058 solved

--- medium/q2058.cpp
+++ medium/q2058.cpp
@@ -12,7 +12,41 @@ struct ListNode {
 };
 
 vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        
+    if (head == nullptr 
+    || head->next == nullptr 
+    || head->next->next == nullptr 
+    || head->next->next->next == nullptr) return {-1,-1};
+
+    ListNode* prev = head;
+    ListNode* tmp = head->next;
+    ListNode* nxt = head->next->next;
+
+    int st = -1;
+    int end = -1;
+
+    int mn = INT_MAX;
+
+    int i = 1;
+    int found = 0;
+    while (nxt != nullptr) {
+        if ((tmp->val > prev->val && tmp->val > nxt->val) || (tmp->val < prev->val && tmp->val < nxt->val)) {
+            found++;
+            if (st == -1) st = i;
+            if (end != -1) mn = min(mn, i-end);
+            end = i;
+        }
+
+        prev = tmp;
+        tmp = nxt;
+        nxt = nxt->next;
+        i++;
+    }
+
+    if (found < 2) return {-1, -1};
+
+    int mx = end - st;
+
+    return {mn, mx};
 }
 
 int main () {

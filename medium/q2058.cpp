5b5f20fda80369c1a65d41291cb5f47218efcf21
dhrxvvnn <dhruvin126gm@gmail.com>
2026-08-31 10:01:51 +0530

    q2058

---
+++ medium/q2058.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+#include <vector>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        
+}
+
+int main () {
+
+    return 0;
+}
